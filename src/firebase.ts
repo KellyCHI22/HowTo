01ee3b5f1666c192abc96094dfc2082a167a4950
Hsin-tingCHI <chihsinting@gmail.com>
2023-05-04 17:24:04 +0200

    init: add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAZ6i_tQSXJuuimCcaPVSvsgNE4h0acQ5Q',
+  authDomain: 'howto-creative.firebaseapp.com',
+  projectId: 'howto-creative',
+  storageBucket: 'howto-creative.appspot.com',
+  messagingSenderId: '938572061197',
+  appId: '1:938572061197:web:4715ccc7d06dca432b9344',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
