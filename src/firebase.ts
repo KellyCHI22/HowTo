c0d85665c0e071c7a683862aba4392caef14f1a8
Hsin-tingCHI <chihsinting@gmail.com>
2023-05-04 18:16:02 +0200

    init: set up firebase services

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,7 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAZ6i_tQSXJuuimCcaPVSvsgNE4h0acQ5Q',
@@ -11,3 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
