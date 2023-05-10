4cfee25056b03bb657ed2a6b0736905cf8a9136c
Hsin-tingCHI <chihsinting@gmail.com>
2023-05-10 18:27:00 +0200

    layout: add SearchPage back button

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,19 @@
-import { RiCloseFill, RiSearchLine } from 'react-icons/ri';
+import {
+  RiArrowLeftLine,
+  RiCloseFill,
+  RiEdit2Line,
+  RiSearchLine,
+} from 'react-icons/ri';
 import Tag from '~/components/elements/Tag';
 import { ReactComponent as SearchIllustration } from '~/assets/illustration_search.svg';
 import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Button from '~/components/elements/Button';
 
 export default function SearchPage() {
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState('');
+
   const tags = [
     'funny',
     'daily life',
@@ -19,7 +28,15 @@ export default function SearchPage() {
   ];
   return (
     <>
-      <nav className="flex h-[4.5rem] items-center justify-between gap-2 px-4 py-1 shadow-basic">
+      <nav className="flex h-[4.5rem] items-center justify-between gap-2 bg-white px-4 py-1 shadow-basic">
+        <button
+          className="mr-1 text-slate-400"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <RiArrowLeftLine className="text-2xl" />
+        </button>
         <div className="flex w-full items-center rounded-full bg-gray-200 px-2 focus-within:ring-2 focus-within:ring-teal-400">
           <button className="text-teal-500">
             <RiSearchLine className="text-2xl" />
@@ -27,16 +44,14 @@ export default function SearchPage() {
 
           <input
             type="text"
-            className="flex-1 border-none bg-transparent placeholder-slate-400 focus:outline-none focus:ring-0"
+            id="searchQuery"
+            value={searchQuery}
             placeholder="search keywords, tags..."
+            className="flex-1 border-none bg-transparent placeholder-slate-400 focus:outline-none focus:ring-0"
             autoFocus
+            onChange={(e) => setSearchQuery(e.target.value)}
           />
-          <button
-            className="text-slate-400"
-            onClick={() => {
-              navigate(-1);
-            }}
-          >
+          <button className="text-slate-400" onClick={() => setSearchQuery('')}>
             <RiCloseFill className="text-2xl " />
           </button>
         </div>
@@ -55,6 +70,16 @@ export default function SearchPage() {
         <div className="mt-24 grid place-items-center">
           <SearchIllustration />
         </div>
+        <Button
+          loading={false}
+          rounded
+          primary
+          className="fixed bottom-8 right-5 px-3 py-3 shadow-lg"
+        >
+          <Link to="/create">
+            <RiEdit2Line className="text-2xl" />
+          </Link>
+        </Button>
       </div>
     </>
   );
