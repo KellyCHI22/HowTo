6ea888d80e018e99a0f93ca4bf4387e8f8e230e2
Hsin-tingCHI <chihsinting@gmail.com>
2023-05-16 11:59:35 +0200

    layout: add desktop sidebar basic styles

--- src/components/layouts/HowToLayout.tsx
+++ src/components/layouts/HowToLayout.tsx
@@ -1,17 +1,133 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import {
+  RiLightbulbFlashLine,
+  RiLightbulbFlashFill,
+  RiBookmark2Line,
+  RiBookmark2Fill,
+  RiAccountCircleLine,
+  RiAccountCircleFill,
+  RiSettings5Line,
+  RiSettings5Fill,
+  RiHeartLine,
+  RiChat1Line,
+} from 'react-icons/ri';
+import { Outlet } from 'react-router-dom';
+import { AppNavLink } from '../MobileSidebar';
 
 export default function HowToLayout() {
   return (
     <>
-      {/* <aside>
-        This is the sidebar
-        <NavLink to="/howtos">Explore</NavLink>
-        <NavLink to="/bookmarks">Bookmarks</NavLink>
-        <NavLink to="/settings">Settings</NavLink>
-      </aside> */}
-      <main>
-        <Outlet />
-      </main>
+      <div className="container mx-auto flex items-start">
+        <aside className="hidden space-y-5 md:m-5 md:my-12 md:block">
+          <AsideNavLinks />
+          <AsideTopUsers />
+          <AsideLatestHowTo />
+        </aside>
+        <main className="flex-1">
+          <Outlet />
+        </main>
+      </div>
     </>
+  );
+}
+
+function AsideNavLinks() {
+  return (
+    <div className="w-64 rounded-xl bg-white p-3 shadow-basic">
+      <ul className="space-y-2 font-bold">
+        <li>
+          <AppNavLink
+            to="/howtos"
+            label="Explore"
+            defaultIcon={<RiLightbulbFlashLine />}
+            activeIcon={<RiLightbulbFlashFill />}
+          />
+        </li>
+        <li>
+          <AppNavLink
+            to="/bookmarks"
+            label="Bookmarks"
+            defaultIcon={<RiBookmark2Line />}
+            activeIcon={<RiBookmark2Fill />}
+          />
+        </li>
+        <li>
+          <AppNavLink
+            to="/users/123"
+            label="Profile"
+            defaultIcon={<RiAccountCircleLine />}
+            activeIcon={<RiAccountCircleFill />}
+          />
+        </li>
+        <li>
+          <AppNavLink
+            to="/settings"
+            label="Settings"
+            defaultIcon={<RiSettings5Line />}
+            activeIcon={<RiSettings5Fill />}
+          />
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function AsideLatestHowTo() {
+  return (
+    <div className="w-64 rounded-xl bg-white shadow-basic">
+      <h3 className="border-b border-b-gray-200 px-5 py-3 text-lg font-bold text-teal-500">
+        Latest How To
+      </h3>
+      <div>
+        <LatestHowToItem />
+        <LatestHowToItem />
+        <LatestHowToItem />
+      </div>
+    </div>
+  );
+}
+
+function LatestHowToItem() {
+  return (
+    <div className="px-5 py-3">
+      <h4 className="mb-1 font-bold">How to turn your cat into a DJ?</h4>
+      <div className="flex items-center justify-start gap-3 text-gray-400">
+        <div className="flex items-center gap-1">
+          <RiChat1Line />5
+        </div>
+        <div className="flex items-center gap-1">
+          <RiHeartLine />6
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function AsideTopUsers() {
+  return (
+    <div className="w-64 rounded-xl bg-white shadow-basic">
+      <h3 className="border-b border-b-gray-200 px-5 py-3 text-lg font-bold text-teal-500">
+        Top Users
+      </h3>
+      <div>
+        <AsideTopUserItem />
+        <AsideTopUserItem />
+        <AsideTopUserItem />
+      </div>
+    </div>
+  );
+}
+
+function AsideTopUserItem() {
+  return (
+    <div className="flex items-center gap-2 px-5 py-3">
+      <img
+        src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
+        alt=""
+        className="aspect-square h-9 w-9 rounded-full object-cover"
+      />
+      <div>
+        <p className="font-bold">Betty Liang</p>
+      </div>
+    </div>
   );
 }
