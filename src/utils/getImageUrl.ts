3043acb4e18544910d2117790299e73c637ca807
Hsin-tingCHI <chihsinting@gmail.com>
2023-06-02 11:18:00 +0200

    feat: add browser-image-compression package for image compression

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,14 +1,22 @@
+import imageCompression from 'browser-image-compression';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { storage } from '~/firebase';
+
+const compressOptions = {
+  maxSizeMB: 1,
+  maxWidthOrHeight: 1920,
+  useWebWorker: true,
+};
 
 const getImageUrl = async (imageFile: File | null, path: string) => {
   if (imageFile !== null) {
     try {
+      const compressedFile = await imageCompression(imageFile, compressOptions);
       const imageRef = ref(
         storage,
-        `${path}/${imageFile.name + crypto.randomUUID()}`
+        `${path}/${compressedFile.name + crypto.randomUUID()}`
       );
-      const snapshot = await uploadBytes(imageRef, imageFile);
+      const snapshot = await uploadBytes(imageRef, compressedFile);
       const url = await getDownloadURL(snapshot.ref);
       return url;
     } catch (error) {
