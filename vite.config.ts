54de7d40cef34f3d8d5cb41d73dd1b0c7d875771
Hsin-tingCHI <chihsinting@gmail.com>
2023-06-23 11:56:11 +0200

    test: config vitest and react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -6,6 +9,11 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
